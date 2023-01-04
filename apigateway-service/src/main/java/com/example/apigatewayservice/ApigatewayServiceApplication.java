5821639ec0d0b1c966df34c028a29b3234e30598
Jeonwoojae <dnwo0719@icloud.com>
2023-01-05 03:28:15 +0900

    git repository로 설정 관리, actuator/refresh 404 error

--- apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
@@ -10,4 +13,9 @@ public class ApigatewayServiceApplication {
 		SpringApplication.run(ApigatewayServiceApplication.class, args);
 	}
 
+//	각 서비스가 호출되는 상태를 보여주는 actiator/httptrace를 위해 구현
+	@Bean
+	public HttpTraceRepository httpTraceRepository(){
+		return new InMemoryHttpTraceRepository();
+	}
 }
