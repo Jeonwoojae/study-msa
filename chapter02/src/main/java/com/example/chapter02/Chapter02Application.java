6e842fe483eadc34723f08b48e5d826d03f5782e
Jeonwoojae <dnwo0719@icloud.com>
2023-03-07 15:03:34 +0900

    [feat]:환경변수와 빈이름 출력하기

--- chapter02/src/main/java/com/example/chapter02/Chapter02Application.java
+++ chapter02/src/main/java/com/example/chapter02/Chapter02Application.java
@@ -1,13 +1,26 @@
 package com.example.chapter02;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.util.Arrays;
+
+@Slf4j
 @SpringBootApplication
 public class Chapter02Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Chapter02Application.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(Chapter02Application.class, args);
+
+        Environment env = ctx.getBean(Environment.class);
+        String portValue = env.getProperty("server.port");
+        log.info("Customized Port : {}",portValue);
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.stream(beanNames).forEach(name -> log.info("Bean Name : {}", name));
     }
 
 }
