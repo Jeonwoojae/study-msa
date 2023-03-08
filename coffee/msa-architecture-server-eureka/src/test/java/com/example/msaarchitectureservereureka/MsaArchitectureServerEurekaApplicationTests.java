4f516143d093daefe96f5c59a46f1f96e96bb57f
Jeonwoojae <dnwo0719@icloud.com>
2023-03-08 17:17:31 +0900

    [feat]:eureka server 생성, zuul은 버전 호환 문제로 gateway로 변경

---
+++ coffee/msa-architecture-server-eureka/src/test/java/com/example/msaarchitectureservereureka/MsaArchitectureServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaarchitectureservereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaArchitectureServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
