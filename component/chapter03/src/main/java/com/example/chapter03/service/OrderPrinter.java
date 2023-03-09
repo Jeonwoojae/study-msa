f1796fcf4084877c361c9a1c80437ec5de0889aa
Jeonwoojae <dnwo0719@icloud.com>
2023-03-09 15:12:57 +0900

    [feat]:의존성 주입 예제

---
+++ component/chapter03/src/main/java/com/example/chapter03/service/OrderPrinter.java
@@ -0,0 +1,45 @@
+package com.example.chapter03.service;
+
+import com.example.chapter03.domain.ProductOrder;
+import com.example.chapter03.domain.format.Formatter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.StringJoiner;
+
+@Slf4j
+@Service
+public class OrderPrinter implements Printer<ProductOrder> {
+//    Field Injection
+    @Autowired
+    @Qualifier("localDateTimeFormatter")
+    private Formatter formatter01;
+
+    private Formatter formatter02;
+//    Setter Method Injection
+    @Autowired
+    public void setFormatter02(@Qualifier("LocalDateTimeFormatter") Formatter formatter) {
+        this.formatter02 = formatter;
+    }
+
+    private Formatter formatter;
+//    Constructor Injection
+    @Autowired
+    public OrderPrinter(@Qualifier("localDateTimeFormatter") Formatter formatter) {
+        this.formatter = formatter;
+    }
+
+    @Override
+    public void print(OutputStream os, ProductOrder productOrder) throws IOException {
+        StringJoiner joiner = new StringJoiner("\r\n");
+        joiner.add(productOrder.getBuyerName());
+        joiner.add(productOrder.getOrderAmount().toPlainString());
+        joiner.add(formatter.of(productOrder.getOrderAt()));
+
+        os.write(joiner.toString().getBytes());
+    }
+}
